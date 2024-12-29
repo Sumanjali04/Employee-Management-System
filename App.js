b54bf7b23d3b817955b09aa0fa9dd62483a13b63
Sai Sumanjali P M <94740454+Sumanjali04@users.noreply.github.com>
2024-12-29 20:21:32 +0530

    App.js

--- App.js
+++ App.js
@@ -1,32 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import AddEmployee from './pages/AddEmployee';
 import SearchEmployee from './pages/SearchEmployee';
 import UpdateEmployee from './pages/UpdateEmployee';
 import ViewEmployees from './pages/ViewEmployees';
 
 function App() {
-    return (
-      <Router>
-        <div>
-          <h1>Employee Management System</h1>
-          <nav>
-            <ul>
-              <li><Link to="/add">Add Employee</Link></li>
-              <li><Link to="/search">Search Employee</Link></li>
-              <li><Link to="/update">Update Employee</Link></li>
-              <li><Link to="/view">View All Employees</Link></li> {/* New link */}
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/add" element={<AddEmployee />} />
-            <Route path="/search" element={<SearchEmployee />} />
-            <Route path="/update" element={<UpdateEmployee />} />
-            <Route path="/view" element={<ViewEmployees />} /> {/* New route */}
-          </Routes>
-        </div>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Patient Record System</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/add">Add Employee</Nav.Link>
+            <Nav.Link as={Link} to="/search">Search Employee</Nav.Link>
+            <Nav.Link as={Link} to="/update">Update Employee</Nav.Link>
+            <Nav.Link as={Link} to="/view">View Employees</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <Routes>
+          <Route path="/add" element={<AddEmployee/>} />
+          <Route path="/search" element={<SearchEmployee/>} />
+          <Route path="/update" element={<UpdateEmployee />} />
+          <Route path="/view" element={<ViewEmployees />} />
+        </Routes>
+      </Container>
+    </Router>
+  );
+}
 
 export default App;
+
+          
